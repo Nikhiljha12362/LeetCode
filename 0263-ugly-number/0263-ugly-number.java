6adfe417cb7d04717dfcc82283115b3041c79433
Nikhil kumar jha <nikhiljha844@gmail.com>
2026-08-15 20:29:39 +0530

    Update 0263-ugly-number.java
    impor must be read

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -3,7 +3,7 @@ class Solution {
         if(n<=0){
             return false;
         }
-    while(n%2==0){
+    while(n%2==0){  // these are the remove condition see if in the question certain factor are allowed then remove that factor by writing this
         n= n/2;
     }
     while(n%3==0){
@@ -15,3 +15,13 @@ class Solution {
     return n==1;
 }
 }
+ // Nootes for this pattern Question mein:
+// "Only certain factors allowed"
+
+//         ↓
+
+// Repeatedly divide/remove those allowed factors
+
+//         ↓
+
+// Check what's left
