6d9290011b87d34727afadaa120e73874d4858fb
Nikhil kumar jha <nikhiljha844@gmail.com>
2026-08-20 19:49:15 +0530

    Time: 4 ms (5.03%), Space: 268.1 MB (5.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n  = nums.length;
-         k =k%n;
-        int [] temp = new int[n];
-        for(int i =0;i<n;i++) {
-            temp[(i+k)%n] = nums[i]; // temp[5,6,7,1,2,3,4,]
-            
-        }
-        // temp array  se nums array  mei coppy krne ke liye 
-        for(int i=0;i<n;i++){
-         nums[i] = temp[i];
-        }
+        int n = nums.length;
+        k = k % n;
 
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
 
-        
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            i++;
+            j--;
+        }
     }
 }
